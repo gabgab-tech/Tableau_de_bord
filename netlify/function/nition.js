09f239a903200189622b1c453880bbb2653be682
gabgab-tech <gabrielle.gauvreaug@gmail.com>
2026-06-07 20:40:28 -0400

    Add Notion API handler function

---
+++ netlify/function/nition.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const { endpoint, body } = JSON.parse(event.body);
+  const res = await fetch(`https://api.notion.com/v1/${endpoint}`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+  const data = await res.json();
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify(data)
+  };
+};
